cbcb608edec4bfad3f2f96ce097fa4b8affb0573
shuangshuangbb <17600930534@163.com>
2022-10-20 16:08:35 +0800

    Site updated: 2022-10-20 16:08:35

--- js/sun_moon.js
+++ js/sun_moon.js
@@ -15,9 +15,11 @@ function switchNightMode() {
     activateDarkMode()
     saveToLocal.set('theme', 'dark', 2)
     GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+    document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
   } else {
     activateLightMode()
     saveToLocal.set('theme', 'light', 2)
+    document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
   }
   // handle some cases
   typeof utterancesTheme === 'function' && utterancesTheme()
